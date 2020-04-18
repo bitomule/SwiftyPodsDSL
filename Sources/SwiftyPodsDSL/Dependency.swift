876499be69c5e04b738cc581eed9bd21c11e2a1d
David Collado <david.collado@wallapop.com>
2020-04-18 13:45:33 +0200

    Rewrite to match Package style

--- Sources/SwiftyPodsDSL/Dependency.swift
+++ Sources/SwiftyPodsDSL/Dependency.swift
@@ -1,17 +1,33 @@
 import Foundation
 
-public struct Dependency {
-    public let name: String
-    public let version: String
-    
-    public init(name: String, version: String) {
-        self.name = name
-        self.version = version
+public extension Podfile.Target {
+    public struct Dependency {
+        public let name: String
+        public let version: String?
+        
+        fileprivate init(name: String, version: String?) {
+            self.name = name
+            self.version = version
+        }
     }
 }
 
-extension Dependency {
+public extension Podfile.Target.Dependency {
     public func toString() -> String {
-        "pod '\(name)', '\(version)'"
+        var podLine = "pod \(name)"
+        if let version = version {
+            podLine += ", \(version)"
+        }
+        return podLine
+    }
+}
+
+public extension Podfile.Target.Dependency {
+    static func dependency(name: String, version: String) -> Podfile.Target.Dependency {
+        Podfile.Target.Dependency(name: name, version: version)
+    }
+    
+    static func dependency(name: String) -> Podfile.Target.Dependency {
+        Podfile.Target.Dependency(name: name, version: nil)
     }
 }
--- Sources/SwiftyPodsDSL/Target.swift
+++ Sources/SwiftyPodsDSL/Target.swift
@@ -1,17 +1,28 @@
 import Foundation
 
-public struct Target {
-    public let name: String
-    public let dependencies: [Dependency]
-    public let childTargets: [Target]
-    
-    public init(
-        name: String,
-        dependencies: [Dependency],
-        childTargets: [Target] = []
-    ) {
-        self.name = name
-        self.dependencies = dependencies
-        self.childTargets = childTargets
+public extension Podfile {
+    public class Target {
+        public let name: String
+        public let project: String
+        public let dependencies: [Dependency]
+        public let childTargets: [Target]
+        
+        fileprivate init(
+            name: String,
+            project: String,
+            dependencies: [Dependency],
+            childTargets: [Target] = []
+        ) {
+            self.name = name
+            self.project = project
+            self.dependencies = dependencies
+            self.childTargets = childTargets
+        }
+    }
+}
+
+public extension Podfile.Target {
+    public static func target(name: String, project: String, dependencies: [Dependency]) -> Podfile.Target {
+        Podfile.Target(name: name, project: project, dependencies: dependencies)
     }
 }
